220166efe7a33d32b6c037ec3f4ed40f71754525
Areen Kaur <46225470+areenoverclouds@users.noreply.github.com>
2022-12-10 13:19:45 +0530

    Add files via upload

---
+++ day10_1.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*----------------------------------------------------------------------------*/
+
+void solve(){
+    int x = 1, ans = 0, cycle = 0;
+    // end the input with . in newline
+    while(true){
+        string task;
+        cin >> task;
+        if(task == ".") break;
+        else if(task == "noop"){
+            cycle++;
+            // check
+            if(cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || 
+               cycle == 180 || cycle == 220)
+               ans += (x * cycle);
+        } 
+        else{
+            int amt; cin >> amt;
+            cycle++;
+            // check
+            if(cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || 
+               cycle == 180 || cycle == 220)
+               ans += (x * cycle);
+            cycle++;
+            // check
+            if(cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || 
+               cycle == 180 || cycle == 220)
+               ans += (x * cycle);
+            x += amt;
+        }
+    }
+    cout << ans;
+}
+
+/*----------------------------------------------------------------------------*/
+signed main() {
+    
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); cout.tie(NULL);
+    
+    solve();
+    
+	return 0;
+}
